c53de86b1206993fc285334f82c986a34a1249c4
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-09-14 12:22:20 +0200

    Ajout du composent NavBar

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import Navbar from "./components/NavBar";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Simplon Cine</h1>
-      <h2>Discover our movies</h2>
-    </div>
+    <>
+      <Navbar />
+
+
+
+    </>
+
   );
 }
